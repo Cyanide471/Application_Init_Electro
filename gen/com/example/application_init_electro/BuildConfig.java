6b29d669961ea119e04f376858686e984c6e066b
Cyanide471 <c1sainsa@enib.fr>
2014-05-01 15:08:13 +0200

    message de commit

---
+++ gen/com/example/application_init_electro/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.application_init_electro;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
